8c425088298854c296e2d01f574ba728307f19c7
Welvis <welvisconta@gmail.com>
2023-11-27 11:44:27 -0300

    rede social em andamento

--- semana3/atividade_aula/RedeSocial/src/RedeSocial/Sessao.java
+++ semana3/atividade_aula/RedeSocial/src/RedeSocial/Sessao.java
@@ -10,11 +10,29 @@ public class Sessao {
 		this.dataHoraInicio = dataHoraInicio;
 		this.dataHoraFim = dataHoraFim;
 	}
+	public Sessao() {}
 	
 	private Date dataHoraInicio;
 	private Date dataHoraFim;
+	private boolean ativo = false;
+	private Usuario usuario;
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 	
-	public Sessao() {}
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+	
+	public void ativarSessao() {
+		this.ativo = true;
+	}
+	
+	public void desativarSessao() {
+		this.ativo = false;
+	}
+	
 
 	public Date getDataHoraInicio() {
 		return dataHoraInicio;
@@ -32,23 +50,135 @@ public class Sessao {
 		this.dataHoraFim = dataHoraFim;
 	}
 
-	public boolean autenticar(ArrayList<Usuario> listaDeUsuarios) {
+	public boolean logar(ArrayList<Usuario> listaDeUsuarios) {
 		Scanner scan = new java.util.Scanner(System.in);
 		System.out.println("Digite seu usuário:\n");
 		String user = scan.nextLine();
 		System.out.println("Digite sua senha:\n");
 		String pass = scan.nextLine();
+		
+		Sessao sessao = new Sessao();
 
 		for (int i = 0; i < listaDeUsuarios.size(); i++) {
 			if (user.equals(listaDeUsuarios.get(i).getEmail()) && pass.equals(listaDeUsuarios.get(i).getSenha())) {
+				sessao.ativarSessao();
+				sessao.setDataHoraInicio(new Date());
+				setUsuario(listaDeUsuarios.get(i));
+				scan.close();
+				return true;
+			}
+		}
+		
+		scan.close();
+		return false;
+	}
+	
+		
+	public void deslogar(Sessao sessao) {
+		sessao.desativarSessao();
+		sessao.setDataHoraFim(new Date());
+	}
+
+	public boolean isAtivo() {
+		return this.ativo;
+	}
+
+	public Boolean solicitarAutenticacao(ArrayList<Usuario> listaDeUsuarios) {
+		Scanner scan = new java.util.Scanner(System.in);
+		System.out.println("Digite seu usuário:\n");
+		String user = scan.nextLine();
+		System.out.println("Digite sua senha:\n");
+		String pass = scan.nextLine();
+		
+		Sessao sessao = new Sessao();
+
+		for (int i = 0; i < listaDeUsuarios.size(); i++) {
+			if (user.equals(listaDeUsuarios.get(i).getEmail()) && pass.equals(listaDeUsuarios.get(i).getSenha())) {
+				sessao.ativarSessao();
+				sessao.setDataHoraInicio(new Date());
+				setUsuario(listaDeUsuarios.get(i));
+				scan.close();
+				return true;
+			}
+		}
+		
+		scan.close();
+		return false;
+	}
+
+	public void criarAmizade(Usuario user1, Usuario user2){
+		user1.setAmizade(user2);
+
+	}
+
+	public Boolean defazerAmizade(Usuario user1, Usuario user2){
+		for (int i = 0; i < user1.getListaAmizade().size(); i++) {
+			
+			if(user2.equals(user1.getListaAmizade().get(i))){
+				user1.getListaAmizade().remove(i);
 				return true;
 			}
 		}
 		return false;
+
+	}
+
+	public void criarPostagem(Usuario usuario){
+		Scanner scan = new java.util.Scanner(System.in);
+		System.out.println("Digite sua postagem:\n");
+		String user = scan.nextLine();
+		usuario.setPostagem(user);
+		scan.close();
+	}
+
+	public void listarPostagem(Usuario usuario){
+		for (int i = 0; i < usuario.getListaDEPostagens().size(); i++) {
+			System.out.println("Postagem: " + usuario.getListaDEPostagens().get(i));
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+		
+		Sessao sessao = new Sessao();
+		
+		Scanner scan = new Scanner(System.in);
+        int opcao = 0;
+        
+        while (opcao != 4) {
+            System.out.println("Menu:");
+            System.out.println("1. Logar");
+            System.out.println("2. Finalizar sessao");
+            System.out.println("3. Listar postagens");
+			System.out.println("4. Fazer postagem");
+            System.out.println("5. Sair");
+            System.out.println("Escolha uma opcao: ");
+            opcao = scan.nextInt();
+            
+            switch (opcao) {
+                case 1:
+                	sessao.logar(usuarios);
+                    break;
+                case 2:
+                	sessao.deslogar(sessao);
+                    break;
+                case 3:
+                	sessao.listarPostagem(sessao.getUsuario());
+                    break;
+
+				case 4:
+					sessao.criarPostagem(sessao.getUsuario());
+                case 5:
+                    System.out.println("Saindo do programa...");
+                    break;
+                default:
+                    System.out.println("Opcao invalida! Tente novamente.");
+                    break;
+            }
+        }
+        
+        scan.close();
 
 	}
 
--- semana3/atividade_aula/RedeSocial/src/RedeSocial/Usuario.java
+++ semana3/atividade_aula/RedeSocial/src/RedeSocial/Usuario.java
@@ -5,20 +5,36 @@ import java.util.ArrayList;
 
 public class Usuario {
 	
-	public Usuario(String nome, String email, String senha, ArrayList<String> listaDEPostagens) {
+	public Usuario(String nome, String email, String senha, ArrayList<String> listaDePostagens) {
 		super();
 		this.nome = nome;
 		Email = email;
 		this.senha = senha;
-		this.listaDEPostagens = listaDEPostagens;
+		this.listaDePostagens = listaDePostagens;
 	}
 	public Usuario(String nome, String email) {
 		super();
 		this.nome = nome;
 		Email = email;
 	}
+
+	private String nome;
+	private String Email;
+	private String senha;
+	private ArrayList<String> listaDePostagens;
+	private ArrayList<Usuario> listaDeAmigos;
+	
+	
 	
 	public Usuario() {}
+	
+	public void setAmizade(Usuario user) {
+		this.listaDeAmigos.add(user);
+	}
+
+	public ArrayList<Usuario>  getListaAmizade() {
+		return this.listaDeAmigos;
+	}
 	
 	public String getNome() {
 		return nome;
@@ -45,29 +61,36 @@ public class Usuario {
 	}
 
 	public ArrayList<String> getListaDEPostagens() {
-		return listaDEPostagens;
+		return listaDePostagens;
 	}
 
-	public void setListaDEPostagens(ArrayList<String> listaDEPostagens) {
-		this.listaDEPostagens = listaDEPostagens;
+	public void setListaDePostagens(ArrayList<String> listaDePostagens) {
+		this.listaDePostagens = listaDePostagens;
+	}
+	
+	public void setPostagem(String postagem) {
+		this.listaDePostagens.add(postagem);
 	}
 	
 	public void novaPostagem(String postagem) {
-		this.listaDEPostagens.add(postagem);
+		this.listaDePostagens.add(postagem);
 	}
 	
 	public void criarAmizade(Usuario amigo) {
-		
+		setAmizade(amigo);
 	}
 	
-	public void destruirAizade(Usuario amigo) {
-		
+	public Boolean destruirAizade(Usuario amigo) {
+		for (int i = 0; i < getListaAmizade().size(); i++) {
+			
+			if(amigo.equals(getListaAmizade().get(i))){
+				getListaAmizade().remove(i);
+				return true;
+			}
+		}
+		return false;
 	}
 
-	private String nome;
-	private String Email;
-	private String senha;
-	private ArrayList<String> listaDEPostagens;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
