5d933a87624d5cbb5c0e6a7a5e63f1feb0d40b38
IpvLeo <leoanarcopunk77@gmail.com>
2023-12-15 01:29:21 -0300

    tentando

--- Semana1/Carinho de Compras/CarinhoDeCompras/src/CarrinhoDeCompras.java
+++ Semana1/Carinho de Compras/CarinhoDeCompras/src/CarrinhoDeCompras.java
@@ -6,6 +6,7 @@ public class CarrinhoDeCompras {
     Cliente cliente;
     ArrayList<Itens> itens;
     boolean compraFechada = false;
+
     
     
 
@@ -22,13 +23,13 @@ public class CarrinhoDeCompras {
 
 
 
-    public void adicionarItem(Itens item, int quantidade) 
+    public void adicionarItem(Itens item) 
     throws ContaClosedException{
 
         if(!compraFechada){
-            for(int i=0; i<quantidade; i++){
+            for(int i=0; i<item.getQuantidade(); i++){
                 this.itens.add(item);
-                System.out.println(quantidade +" " + item.descricao + " >> Adicionado ao carrinho <<");
+                System.out.println(item.getQuantidade() +" " + item.descricao + " >> Adicionado ao carrinho <<" + cliente.nome);
             }
         }
         else{
@@ -38,12 +39,12 @@ public class CarrinhoDeCompras {
 
     }
 
-    public void removerItem(Itens item, int quantidade)  
+    public void removerItem(Itens item)  
     throws ContaClosedException{
         if(!compraFechada){
-        for(int i = 0; i < quantidade; i++){
+        for(int i = 0; i < item.getQuantidade(); i++){
             this.itens.remove(item);
-            System.out.println(quantidade  +" "+ item.descricao + " >> Removido do carrinho <<");
+            System.out.println( " "+ item.getDescricao()+ " >> Removido do carrinho <<" + cliente.nome);
            
          }
         }else{
@@ -65,14 +66,17 @@ public class CarrinhoDeCompras {
         }
 
 
-    public void CalcularTotalCompra(Itens item, int quantidade){
-        for(int i=0; i < quantidade; i++){
-            quantidade *= item
-
+    public void CalcularTotalCompra(Itens item){
+        double total = 0;
+        for (int i = 0; i < item.getQuantidade(); i++) {
+            total = item.getQuantidade() * item.getPreco()  ;
+              System.out.println("O valor da compra é: " + total );
+              
         }
+       
     }
         
-
+    
     
 
     public static void main(String[] args) throws ContaClosedException {
@@ -83,25 +87,31 @@ public class CarrinhoDeCompras {
         System.out.println(carrinho.cliente.nome);
 
 
-        Itens item1 = new Itens("Jaqueta", 21, 122.30);
-        Itens item2 = new Itens("sapato", 1, 10.20);
+        Itens item1 = new Itens("Jaqueta", 21, 122.30, 2);
+        Itens item2 = new Itens("sapato", 1, 10.20, 3);
         
-        carrinho.adicionarItem(item1, 2);
+        carrinho.adicionarItem(item1);
 
-        carrinho.removerItem(item1, 1);
+        carrinho.removerItem(item1);
+       
+        
+        carrinho.adicionarItem(item2);
+
+        carrinho.CalcularTotalCompra(item1);
+
+        carrinho.removerItem(item2);
+
+        
 
         carrinho.fecharCompra();
-        carrinho.adicionarItem(item2, 1);
 
-        carrinho.removerItem(item2, 2);
-
-        carrinho.fecharCompra();
+        carrinho.CalcularTotalCompra(item1);
         }catch(ContaClosedException e ){
         System.out.println("Erro " + e.getMessage());  
         }
     
     
-   
+    }
     
  
    
@@ -110,4 +120,4 @@ public class CarrinhoDeCompras {
 
 }
 
-}
+
--- Semana1/Carinho de Compras/CarinhoDeCompras/src/Itens.java
+++ Semana1/Carinho de Compras/CarinhoDeCompras/src/Itens.java
@@ -1,24 +1,62 @@
 
 public class Itens {
 
+    
     String descricao;
     int id;
     double preco;
+    public int quantidade;
 
-    
 
-    
-    public Itens(String descricao, int id, double preco) {
+public Itens(String descricao, int id, double preco, int quantidade) {
         this.descricao = descricao;
         this.id = id;
         this.preco = preco;
+        this.quantidade = quantidade;
     }
 
 
+
+
+
+
+
+
+
+
+
+    
     public String getDescricao() {
         return descricao;
     }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
 
+    
+
+
+
+  
     
 
 
