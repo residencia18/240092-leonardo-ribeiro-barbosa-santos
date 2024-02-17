7702c1ce97a2c9a6f8cc39e496dd5e1333b8f1a8
IpvLeo <leoanarcopunk77@gmail.com>
2024-02-17 17:42:49 -0300

    ok

---
+++ Semana7/P010/src/main/java/dao/ReembolsoDAO.java
@@ -0,0 +1,71 @@
+package dao;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import EmpresaEletrica.Cliente;
+import EmpresaEletrica.Fatura;
+import EmpresaEletrica.Imovel;
+import EmpresaEletrica.Pagamento;
+import EmpresaEletrica.Reembolso;
+
+public class ReembolsoDAO {
+	public static void main(String[] args) {
+		try {
+			
+			
+			Date data = new Date();
+			
+			Date DataFechameto = new Date(12);
+
+			
+			Cliente novoCliente1 = new Cliente(null, "Jaque", 260099043);
+			Imovel novoImovel1 = new Imovel("Centro Z, 566", novoCliente1);
+			
+			Fatura fatura1 = new Fatura(novoImovel1, data);
+			Pagamento pag = new Pagamento(fatura1, 160);
+			Reembolso reembolso1 = new Reembolso(pag);
+			
+			
+		
+			
+		
+
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("academy");
+			EntityManager em = emf.createEntityManager();
+
+			em.getTransaction().begin();
+
+			em.persist(novoCliente1);
+			em.persist(novoImovel1);
+			em.persist(fatura1);
+			em.persist(pag);
+			em.persist(reembolso1);
+			
+			
+			fatura1.registrarLeitura(30, DataFechameto);
+			
+			pag.registraPagamento(DataFechameto);
+			reembolso1.registrarReembolso();
+		
+			em.getTransaction().commit();
+			
+		
+			em.close();
+			emf.close();
+			
+
+			
+			
+			System.out.println("Objetos Pagamento persistidos com sucesso.");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Erro ao conectar ou persistir objetos: " + e.getMessage());
+		}
+	}
+}
+
+
