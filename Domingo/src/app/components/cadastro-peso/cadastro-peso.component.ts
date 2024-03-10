539cbc99250d3ebb885162d0bfc72cfe16f165c7
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2024-03-10 14:14:46 -0300

    Add files via upload

---
+++ Domingo/src/app/components/cadastro-peso/cadastro-peso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-peso',
+  templateUrl: './cadastro-peso.component.html',
+  styleUrls: ['./cadastro-peso.component.css']
+})
+export class CadastroPesoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
