252bcd652a83fdd5bc6f597a099e85b46558a219
IpvLeo <leoanarcopunk77@gmail.com>
2024-03-19 15:46:39 -0300

    fazendo...

--- Aula4/PP004/funcionario/funcionario/src/main/java/com/leo/gerenciamento/funcionario/FuncionarioApplication.java
+++ Aula4/PP004/funcionario/funcionario/src/main/java/com/leo/gerenciamento/funcionario/FuncionarioApplication.java
@@ -1,13 +1,20 @@
 package com.leo.gerenciamento.funcionario;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.leo.gerenciamento.funcionario.model.Funcionario;
 
 @SpringBootApplication
 public class FuncionarioApplication {
 
+	public static final Logger log = LoggerFactory.getLogger(Funcionario.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FuncionarioApplication.class, args);
+		log.info("Application started");
 	}
 
 }
