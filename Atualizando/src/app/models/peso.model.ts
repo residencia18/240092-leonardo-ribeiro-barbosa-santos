1c66f2ecfb0f7ec910a670b739fc5800a27ea5f0
IpvLeo <leoanarcopunk77@gmail.com>
2024-03-20 20:41:32 -0300

    init

--- Atualizando/src/app/models/peso.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Peso {
-    id: number;
-    valor: number;
-    data: Date;
-  }
-  
--- Domingo/src/app/filtro-suinos.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltroSuinosPipe } from './filtro-suinos.pipe';
-
-describe('FiltroSuinosPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltroSuinosPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
