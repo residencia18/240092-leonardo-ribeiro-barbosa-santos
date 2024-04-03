3a35909f778d1f131b763760207bd276be41ad1e
IpvLeo <leoanarcopunk77@gmail.com>
2024-04-02 22:07:12 -0300

    atualizando DTO e tirandoModelMapper

--- Aula9/mockito/src/main/java/com/leo/mockito/dto/UserDTO.java
+++ Aula9/mockito/src/main/java/com/leo/mockito/dto/UserDTO.java
@@ -1,5 +1,21 @@
 package com.leo.mockito.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class UserDTO {
 
+	private Integer id;
+	private String name;
+	private String email;
+	private String password;
+	
+	
 }
--- Aula9/mockito/src/main/java/com/leo/mockito/resources/UserResource.java
+++ Aula9/mockito/src/main/java/com/leo/mockito/resources/UserResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leo.mockito.domain.User;
+import com.leo.mockito.dto.UserDTO;
 import com.leo.mockito.services.UserService;
 
 @RestController
@@ -18,9 +19,18 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id){
-		return ResponseEntity.ok().body(service.findById(id));
-		
+	public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+	    User user = service.findById(id);
+	    UserDTO userDTO = convertToDTO(user); // Método para converter User para UserDTO
+	    return ResponseEntity.ok().body(userDTO);
 	}
 	
+	private UserDTO convertToDTO(User user) {
+	    UserDTO userDTO = new UserDTO();
+	    userDTO.setId(user.getId());
+	    userDTO.setName(user.getName());
+	    userDTO.setEmail(user.getEmail());
+	    userDTO.setPassword(user.getPassword());
+	    return userDTO;
+	}
 }
