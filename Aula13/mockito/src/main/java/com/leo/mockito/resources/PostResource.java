27add53defc26c3d59054eef6a4c723de52654f3
IpvLeo <leoanarcopunk77@gmail.com>
2024-04-19 23:38:00 -0300

    Exercicio 14: colocando Cacheable em um metodo para melhor o ms

--- Aula13/mockito/src/main/java/com/leo/mockito/resources/PostResource.java
+++ Aula13/mockito/src/main/java/com/leo/mockito/resources/PostResource.java
@@ -3,12 +3,14 @@ package com.leo.mockito.resources;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +27,10 @@ public class PostResource {
 	@Autowired
 	private  PostService service;
 	
-	
 	@GetMapping("/user/{userId}/post")
+    @Cacheable(value = "postsCache", key = "#userId")
 	public ResponseEntity<Page<PostDTO>> listAll(
+			@PathVariable Integer userId,
 	        @RequestParam(defaultValue = "0") int page,
 	        @RequestParam(defaultValue = "10") int size) {
 	    Pageable pageable = PageRequest.of(page, size);
